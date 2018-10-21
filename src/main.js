9cd95aba53457ff24cfa0950e1b174da8cc58e30
Sattar Stamkulov <sattar.coder@gmail.com>
2018-10-21 19:31:52 +0600

    fixed: use cors allow origin to all

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 
 import { server } from './interfaces/graphql/server';
 
 const Port = 4000;
 
 var app = express();
+app.use(
+  cors({
+    'origin': '*',
+  })
+)
 app.use('/graphql', server);
 app.listen(Port);
