c384793378cd76bdfd41045c661da8ffd9e23a4e
Sattar Stamkulov <sattar.coder@gmail.com>
2018-10-21 19:33:50 +0600

    fixed: use mock object for timeRecord request

--- src/interfaces/graphql/resolvers/time_record.js
+++ src/interfaces/graphql/resolvers/time_record.js
@@ -1,9 +1,15 @@
-
+let timeRecordMock = {
+  id: "B58CC80A-1BF5-4652-9559-97AC6C6545AD",
+  amount: 60,
+  timestamp: "20-10-2018 17:28",
+  description: "mocked data from resolver"
+}
 
 export function getTimeRecordOwner(timeRecord) {
   return Promise.resolve({ id: timeRecord.owner_id });
 }
 
 export function getTimeRecordByID(id) {
-  return Promise.resolve({ id: id });
+  let data = Object.assign({}, timeRecordMock, { id: id });
+  return Promise.resolve(data);
 }
